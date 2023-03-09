f426b3f2a90b33701bc2e911b4834b1ad1210ba2
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-08 23:13:39 -0300

    Entidades: Base, Entradas. Validações.

---
+++ Contas/Contas.Domain/Entities/Base.cs
@@ -0,0 +1,9 @@
+﻿namespace Contas.Domain.Entities
+{
+    public abstract class Base
+    {
+        public int Id { get; protected set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+    }
+}
---
+++ Contas/Contas.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,14 @@
+﻿namespace Contas.Domain.Validation
+{
+    public class DomainValidation : Exception
+    {
+        public DomainValidation(string error) : base(error)
+        { }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+                throw new DomainValidation(error);
+        }
+    }
+}
