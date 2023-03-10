44ee91f7f1b7d9270497d64021c1bb9e6244a414
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-10 17:53:47 -0300

    Criação das interfaces

---
+++ Contas/Contas.Domain/Interfaces/IEntradasRepository.cs
@@ -0,0 +1,15 @@
+﻿using Contas.Domain.Entities;
+
+namespace Contas.Domain.Interfaces
+{
+    public interface IEntradasRepository
+    {
+        Task<Entradas> GetByIdAsync(int? id);
+        Task<IEnumerable<Entradas>> GetAllAsync();
+
+        Task<Entradas> CreateAsync(Entradas entradas);
+        Task<Entradas> RemoveAsync(Entradas entradas);
+        Task<Entradas> UpdateAsync(Entradas entradas);
+        Task<Entradas> DeleteAsync(Entradas entradas);
+    }
+}
---
+++ Contas/Contas.Domain/Interfaces/IRecebedoresRepository.cs
@@ -0,0 +1,15 @@
+﻿using Contas.Domain.Entities;
+
+namespace Contas.Domain.Interfaces
+{
+    public interface IRecebedoresRepository
+    {
+        Task<Recebedores> GetByIdAsync(int? id);
+        Task<IEnumerable<Recebedores>> GetAllAsync();
+
+        Task<Recebedores> CreateAsync(Recebedores recebedores);
+        Task<Recebedores> RemoveAsync(Recebedores recebedores);
+        Task<Recebedores> UpdateAsync(Recebedores recebedores);
+        Task<Recebedores> DeleteAsync(Recebedores recebedores);        
+    }
+}
