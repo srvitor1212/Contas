81ad0c2dc517c870703734a5e8b0b865985bedc6
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-14 15:57:39 -0300

    PagamentosConfiguration

--- Contas/Contas.Infra.Data/EntitiesConfiguration/PagamentosConfiguration.cs
+++ Contas/Contas.Infra.Data/EntitiesConfiguration/PagamentosConfiguration.cs
@@ -1,6 +1,16 @@
-﻿namespace Contas.Infra.Data.EntitiesConfiguration
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Contas.Domain.Entities;
+
+namespace Contas.Infra.Data.EntitiesConfiguration
 {
-    internal class PagamentosConfiguration
+    public class PagamentosConfiguration : IEntityTypeConfiguration<Pagamentos>
     {
+        public void Configure(EntityTypeBuilder<Pagamentos> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.TipoDePagamento).HasMaxLength(100).IsRequired();
+        }
     }
 }
