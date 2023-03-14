9c4b98170c37cbfe7444d47ad51f945fde3c21c3
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-14 16:01:57 -0300

    EntradasConfiguration

--- Contas/Contas.Infra.Data/EntitiesConfiguration/EntradasConfiguration.cs
+++ Contas/Contas.Infra.Data/EntitiesConfiguration/EntradasConfiguration.cs
@@ -1,6 +1,17 @@
-﻿namespace Contas.Infra.Data.EntitiesConfiguration
+﻿using Contas.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contas.Infra.Data.EntitiesConfiguration
 {
-    internal class EntradasConfiguration
+    public class EntradasConfiguration : IEntityTypeConfiguration<Entradas>
     {
+        public void Configure(EntityTypeBuilder<Entradas> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Valor).HasPrecision(10, 2).IsRequired();
+        }
     }
 }
