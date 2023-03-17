338f70fe9247cd9584684257fad525b8ba0bbbfa
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-17 10:50:32 -0300

    Ref Application > Domain. Mappers

---
+++ Application/Mapper/DomainToDTO.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapper
+{
+    public class DomainToDTO : Profile
+    {
+        public DomainToDTO() 
+        { 
+            CreateMap<Dividas, DividasDTO>();
+            CreateMap<Entradas, EntradasDTO>();
+            CreateMap<Pagamentos, PagamentosDTO>();
+            CreateMap<Recebedores, RecebedoresDTO>();
+        }
+    }
+}
