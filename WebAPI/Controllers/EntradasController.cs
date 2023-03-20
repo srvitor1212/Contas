f12c8a917eab98e4bacc055e56d9bf65b8425c2b
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-20 11:40:00 -0300

    Iniciando WEBAPI

---
+++ WebAPI/Controllers/EntradasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("teste/[controller]")]
+    public class EntradasController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAll() 
+        {
+            return new string[] { "valor1", "valor2", "valor3" };
+        }
+    }
+}
