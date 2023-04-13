1640ba857d954ac15a8758f2fd6be2c95030b5bc
Vitor Vicente <vitor.email2011@gmail.com>
2023-04-13 11:24:10 -0300

    Adicionando IExceptionFilter

---
+++ WebAPI/Exceptions/FiltrarExceptions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebAPI.Exceptions
+{
+    public class FiltrarExceptions : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
