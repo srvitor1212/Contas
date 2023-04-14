42312e9fb7f673b8454dafb32bb1c179af11d477
Vitor Vicente <vitor.email2011@gmail.com>
2023-04-14 16:14:50 -0300

    Construtor e Setter na entidade Recebedores

--- Domain/Entities/Recebedores.cs
+++ Domain/Entities/Recebedores.cs
@@ -1,9 +1,38 @@
-﻿namespace Domain.Entities
+﻿using Domain.Validation;
+
+namespace Domain.Entities
 {
     public sealed class Recebedores : Base
     {
         public string Nome { get; private set; }
 
         public ICollection<Dividas> Dividas { get; private set; }
+
+        public Recebedores(string nome)
+        {
+            Validation(nome);
+        }
+
+        public void Update(string nome)
+        {
+            Validation(nome);
+        }
+
+        private void Validation(string nome)
+        {
+            DomainValidation.When(
+                string.IsNullOrEmpty(nome),
+                "Nome não pode ser em branco");
+
+            DomainValidation.When(
+                nome.Length < 3,
+                "Nome deve conter ao menos 3 caracteres");
+
+            DomainValidation.When(
+                nome.Length > 200,
+                "Nome é muito longo");
+
+            this.Nome = nome;
+        }
     }
 }
