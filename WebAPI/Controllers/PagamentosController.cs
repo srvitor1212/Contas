79fac7adc415d29bfff86e8f95281a169cc1ff6b
Vitor Vicente <vitor.email2011@gmail.com>
2023-04-17 11:39:42 -0300

    PagamentosController

---
+++ WebAPI/Controllers/PagamentosController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagamentosController : ControllerBase
+    {
+        private readonly IPagamentosServices _pagamentosServices;
+        public PagamentosController(IPagamentosServices pagamentosServices)
+        {
+            this._pagamentosServices = pagamentosServices;
+        }
+    }
+}
