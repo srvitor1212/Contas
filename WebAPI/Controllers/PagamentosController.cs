57ae13cfccbf7c4ea915293c802c6f71824bd244
Vitor Vicente <vitor.email2011@gmail.com>
2023-04-17 11:52:05 -0300

    PagamentosController

--- WebAPI/Controllers/PagamentosController.cs
+++ WebAPI/Controllers/PagamentosController.cs
@@ -1,6 +1,7 @@
-﻿using Application.Interfaces;
-using Microsoft.AspNetCore.Http;
+﻿using Application.DTOs;
+using Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.ViewModel;
 
 namespace WebAPI.Controllers
 {
@@ -12,6 +13,36 @@ namespace WebAPI.Controllers
         public PagamentosController(IPagamentosServices pagamentosServices)
         {
             this._pagamentosServices = pagamentosServices;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PagamentosDTO>>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}", Name = "GetPagamentos")]
+        public async Task<ActionResult<PagamentosDTO>> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] PagamentosViewModel pagamentosViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] PagamentosDTO pagamentosDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ WebAPI/ViewModel/PagamentosViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace WebAPI.ViewModel
+{
+    public class PagamentosViewModel
+    {
+        public string TipoDePagamento { get; private set; }
+    }
+}
