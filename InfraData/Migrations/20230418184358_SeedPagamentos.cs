3fe4774501c898ebbce14ba93ecf61033464ee16
Vitor Vicente <vitor.email2011@gmail.com>
2023-04-19 17:18:39 -0300

    SeedPagamentos inserindo Dinheiro como padrão

--- InfraData/Migrations/20230418184358_SeedPagamentos.cs
+++ InfraData/Migrations/20230418184358_SeedPagamentos.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Globalization;
+using System.Security.Cryptography;
 
 #nullable disable
 
@@ -11,18 +14,49 @@ namespace InfraData.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             DateTime agora = DateTime.Now;
-            string strAgora = agora.ToString();
-            //todo: seed
+            string mes, dia, hora, minuto, segundo;
+            mes =       NormalizeDateTime(agora.Month);
+            dia =       NormalizeDateTime(agora.Day);
+            hora =      NormalizeDateTime(agora.Hour);
+            minuto =    NormalizeDateTime(agora.Minute);
+            segundo =   NormalizeDateTime(agora.Second);
+
+            string strAgora = (
+                agora.Year  + "-" + 
+                mes         + "-" + 
+                dia         + "T" +
+                hora        + ":" + 
+                minuto      + ":" + 
+                segundo     ).ToString();
+
+            // Para permitir setar manualmente a coluna de identificação (PK ou IDENTITY_INSERT)
+            migrationBuilder.Sql(
+                "SET IDENTITY_INSERT Pagamentos ON");
 
             migrationBuilder.Sql(
                 "INSERT INTO Pagamentos(Id, DataCriacao, DataAtualizacao, Nome)" +
                 "VALUES (1, '" + strAgora + "', '" + strAgora + "', 'Dinheiro')");
+
+            // Voltar para OFF por padrão
+            migrationBuilder.Sql(
+                "SET IDENTITY_INSERT Pagamentos OFF");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            //todo: comando de drop table
+        }
 
+        private string NormalizeDateTime(int valor)
+        {
+            string ret = "";
+            if (valor < 10)
+                ret = "0" + valor.ToString();
+            else
+                ret = valor.ToString();
+
+            return ret;
         }
     }
 }
