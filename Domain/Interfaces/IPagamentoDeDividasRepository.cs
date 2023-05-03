507ac119cf11295da186d2b5f43001f0ad4f9e97
Vitor Vicente <vitor.email2011@gmail.com>
2023-05-03 16:07:31 -0300

    IPagamentoDeDividasRepository

---
+++ Domain/Interfaces/IPagamentoDeDividasRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IPagamentoDeDividasRepository
+    {
+        Task<IEnumerable<PagamentoDeDividas>> GetAllAsync(int? idDivida);
+        Task<PagamentoDeDividas> CreateAsync(PagamentoDeDividas pagamentoDeDividas);
+        Task<PagamentoDeDividas> DeleteAsync(PagamentoDeDividas pagamentoDeDividas);
+
+        Task<bool> FoiPago(int idDivida, DateTime date);
+    }
+}
