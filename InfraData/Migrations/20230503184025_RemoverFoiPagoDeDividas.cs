7088a9648735cf1fbf47043da6f7b452ff004e72
Vitor Vicente <vitor.email2011@gmail.com>
2023-05-03 15:41:17 -0300

    Migration remover campo FoiPago

---
+++ InfraData/Migrations/20230503184025_RemoverFoiPagoDeDividas.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfraData.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoverFoiPagoDeDividas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FoiPago",
+                table: "Dividas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "FoiPago",
+                table: "Dividas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
