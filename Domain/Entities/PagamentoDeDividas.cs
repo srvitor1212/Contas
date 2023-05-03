0be2e8225b5dca0ee2c4e5e73ecacbdebbf0c94f
Vitor Vicente <vitor.email2011@gmail.com>
2023-05-03 15:57:39 -0300

    Nova entidade PagamentoDeDividas

---
+++ Domain/Entities/PagamentoDeDividas.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Validation;
+
+namespace Domain.Entities
+{
+    public sealed class PagamentoDeDividas : Base
+    {
+        public DateTime DataEfetivacao { get; private set; }
+
+        public int DividasId { get; set; }
+        public Dividas Dividas { get; set; }
+
+        public PagamentoDeDividas(int id, DateTime dataEfetivacao, int dividaId)
+        {
+            DomainValidation.When(
+                id < 0,
+                "Id inválido");
+            this.Id = id;
+
+            Validation(dataEfetivacao, dividaId);
+        }
+
+        public PagamentoDeDividas(DateTime dataEfetivacao, int dividaId)
+        {
+            Validation(dataEfetivacao, dividaId);
+        }
+
+        public void Validation(DateTime dataEfetivacao, int dividaId)
+        {
+            this.DividasId = dividaId;
+            this.DataEfetivacao = dataEfetivacao;
+        }
+    }
+}
