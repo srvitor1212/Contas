ef185640efa8d9c337d5a4c7e94fdaeed8b6511e
Vitor Vicente <vitor.email2011@gmail.com>
2023-05-04 17:25:41 -0300

    PagamentoDeDividasDTO

---
+++ Application/DTOs/PagamentoDeDividasDTO.cs
@@ -0,0 +1,15 @@
+﻿
+using Domain.Entities;
+
+namespace Application.DTOs
+{
+    public class PagamentoDeDividasDTO
+    {
+        public int Id { get; set; }
+
+        public DateTime DataEfetivacao { get; private set; }
+
+        public int DividasId { get; set; }
+        public Dividas Dividas { get; set; }
+    }
+}
