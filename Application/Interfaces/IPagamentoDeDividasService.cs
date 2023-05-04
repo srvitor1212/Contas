ebe21bdcc424476b650d336868b57d3500dc3942
Vitor Vicente <vitor.email2011@gmail.com>
2023-05-04 17:23:17 -0300

    PagamentoDeDividasService

---
+++ Application/Interfaces/IPagamentoDeDividasService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IPagamentoDeDividasService
+    {
+        Task<IEnumerable<PagamentoDeDividas>> GetAllAsync(int? idDivida);
+        Task<PagamentoDeDividas> CreateAsync(PagamentoDeDividas pagamentoDeDividas);
+        Task<PagamentoDeDividas> DeleteAsync(PagamentoDeDividas pagamentoDeDividas);
+
+        Task<bool> FoiPago(int idDivida, DateTime date);
+    }
+}
---
+++ Application/Services/PagamentoDeDividasService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services
+{
+    public class PagamentoDeDividasService : IPagamentoDeDividasService
+    {
+        public Task<PagamentoDeDividas> CreateAsync(PagamentoDeDividas pagamentoDeDividas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagamentoDeDividas> DeleteAsync(PagamentoDeDividas pagamentoDeDividas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> FoiPago(int idDivida, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PagamentoDeDividas>> GetAllAsync(int? idDivida)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
