3f2cbc089b4f8355e7efd7e7fffe4f2bdbd122df
Vitor Vicente <vitor.email2011@gmail.com>
2023-05-11 16:07:07 -0300

    Controller

---
+++ WebAPI/Controllers/PagamentoDeDividas.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagamentoDeDividas : ControllerBase
+    {
+        private readonly IPagamentoDeDividasService _pagamentoDeDividasService;
+        public PagamentoDeDividas(
+            IPagamentoDeDividasService pagamentoDeDividasService)
+        {
+            this._pagamentoDeDividasService = pagamentoDeDividasService;
+        }
+    }
+}
