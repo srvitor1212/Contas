02b46944c8d99cbba6e08158276d6b582567781f
Vitor - PC <vitor.email2011@gmail.com>
2023-08-09 09:48:57 -0300

    Migration inicial

---
+++ InfraData/Migrations/20230809124704_Inicial-sqlite.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfraData.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicialsqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entradas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Valor = table.Column<double>(type: "REAL", precision: 10, scale: 2, nullable: false),
+                    DataVigencia = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entradas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recebedores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recebedores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dividas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", precision: 10, scale: 2, nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataFim = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DiaVencimento = table.Column<byte>(type: "INTEGER", nullable: false),
+                    PagamentosId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RecebedoresId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dividas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dividas_Pagamentos_PagamentosId",
+                        column: x => x.PagamentosId,
+                        principalTable: "Pagamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Dividas_Recebedores_RecebedoresId",
+                        column: x => x.RecebedoresId,
+                        principalTable: "Recebedores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PagamentoDeDividas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataEfetivacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DividasId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PagamentoDeDividas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PagamentoDeDividas_Dividas_DividasId",
+                        column: x => x.DividasId,
+                        principalTable: "Dividas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dividas_PagamentosId",
+                table: "Dividas",
+                column: "PagamentosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dividas_RecebedoresId",
+                table: "Dividas",
+                column: "RecebedoresId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PagamentoDeDividas_DividasId",
+                table: "PagamentoDeDividas",
+                column: "DividasId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entradas");
+
+            migrationBuilder.DropTable(
+                name: "PagamentoDeDividas");
+
+            migrationBuilder.DropTable(
+                name: "Dividas");
+
+            migrationBuilder.DropTable(
+                name: "Pagamentos");
+
+            migrationBuilder.DropTable(
+                name: "Recebedores");
+        }
+    }
+}
